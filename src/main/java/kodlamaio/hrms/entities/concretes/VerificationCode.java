43027a3866b4d6efa472b6b06696182f97372d40
acer <acer@LAPTOP-68G50CHV.local>
2021-06-14 12:35:26 +0300

    changes

--- src/main/java/kodlamaio/hrms/entities/concretes/VerificationCode.java
+++ src/main/java/kodlamaio/hrms/entities/concretes/VerificationCode.java
@@ -1,28 +1,27 @@
 package kodlamaio.hrms.entities.concretes;
 
-import java.time.LocalDate;
+
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+
+
 import javax.persistence.Table;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name="verification_codes")
 @Data
+@EqualsAndHashCode(callSuper = false)
 @NoArgsConstructor
 @AllArgsConstructor
-public class VerificationCode {
+public class VerificationCode extends Base {
 	
-	@Id
-	@GeneratedValue
-	@Column(name="id")
-	private int id;
+	
 	
 	@Column(name="user_id")
 	private int userId;
@@ -33,15 +32,5 @@ public class VerificationCode {
 	@Column(name="is_confirmed")
 	private boolean isConfirmed;
 	
-	@Column(name="created_at", columnDefinition = "Date defult CURRENT_DATE")
-	private LocalDate createAt = LocalDate.now();
-
-	public VerificationCode(int userId, String code, boolean isConfirmed, LocalDate createAt) {
-		super();
-		this.userId = userId;
-		this.code = code;
-		this.isConfirmed = isConfirmed;
-		this.createAt = createAt;
-	}
-
+	
 }
